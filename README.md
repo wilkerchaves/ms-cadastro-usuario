8ddebc35f27c74f147d060a2c6956247f1e117f5
Wilker Chaves <chaveswill8@gmail.com>
2024-12-28 02:00:10 -0300

    Criacao do projeto

--- README.md
+++
@@ -1,2 +0,0 @@
-# ms-backend-cadastro-usuario
-Repositório do projeto de um micro serviço responsável pelo cadastro de usuário.
---
+++ src/test/java/com/dev/wilkerchaves/ms_cadastro_usuario/MsCadastroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.wilkerchaves.ms_cadastro_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCadastroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
